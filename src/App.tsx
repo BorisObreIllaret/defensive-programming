a86b3ff8f6c0f2f2c2c8e42cdd023edc81c47b00
Boris OBRE-ILLARET <boris.obreillaret@gmail.com>
2024-10-06 10:48:45 +0200

    feat: initial setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import type {FC} from "react";
+
+
+const App: FC = () => {
+	return (
+		<main>
+			<h1>Hello Vite + TypeScript + React + Tailwind + daisyUI</h1>
+		</main>
+	);
+};
+
+export {App};
---
+++ tailwind.config.ts
@@ -0,0 +1,13 @@
+import type {Config} from "tailwindcss";
+import daisyui from "daisyui";
+
+export default {
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		daisyui
+	],
+} satisfies Config;
+
