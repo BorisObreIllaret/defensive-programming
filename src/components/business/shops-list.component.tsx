0510f7a97800db32277135233df5acd6e66fb709
Boris OBRE-ILLARET <boris.obreillaret@gmail.com>
2024-10-06 12:55:23 +0200

    feat: list shops

--- src/components/business/shops-list.component.tsx
+++ src/components/business/shops-list.component.tsx
@@ -1,11 +1,48 @@
 import {type FC} from "react";
+import {useQuery} from "@tanstack/react-query";
+import {ShopService} from "@services/shop.service.ts";
 
 const ShopsListComponent: FC = () => {
+	const {data: shops, error, isLoading} = useQuery({
+		queryKey: ["shops"],
+		queryFn: ShopService.getShops
+	});
+
+	if (error) {
+		return (
+			<div className="mockup-code">
+				<pre className="bg-error text-error-content"><code>Une erreur a été levée lors de la lecture des magasins</code></pre>
+				<pre><code>{error.message}</code></pre>
+			</div>
+		);
+	}
+
 	return (
-		<section className="prose">
-			<h1 className="text-center">Liste des magasins</h1>
+		<section className="flex flex-col gap-4 justify-center">
+			<h1 className="text-center text-2xl text-bold">Liste des magasins</h1>
+			<div className="overflow-x-auto">
+				{isLoading
+					? <div className="skeleton size-80"/>
+					: <table className="table max-w-80 mx-auto">
+						<thead>
+							<tr>
+								<th>Magasin</th>
+							</tr>
+						</thead>
+						<tbody>
+							{(shops?.length) && shops.map(shop => (
+								<tr>
+									<td>{shop}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				}
+			</div>
 		</section>
 	);
 };
 
-export {ShopsListComponent};
+export {
+	ShopsListComponent
+};
